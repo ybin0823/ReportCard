cccb6f4794abcd43fa793bcf3ad10f48454a536f
Jang Young-bin <jyb0823@gmail.com>
2015-09-08 15:27:29 +0900

    Initial Commit

---
+++ ReportCard2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ReportCard2
+//
+//  Created by Jang Young bin on 2015. 9. 8..
+//  Copyright (c) 2015년 Jang Young bin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
